7d2c2a17df5f0dfc23da011d386d62722075901e
kruczek <kruczek86@gmail.com>
2022-12-03 15:28:29 +0100

    add InsertObject for elasticsearch client

--- pkg/repository/elasticsearch_client.go
+++ pkg/repository/elasticsearch_client.go
@@ -1,8 +1,13 @@
 package repository
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"grapefruit/pkg/config"
+	"grapefruit/pkg/model"
 	"log"
 	"net"
 	"net/http"
@@ -41,4 +46,47 @@ func NewElasticSearchClient(cfg config.ElasticSearchDS) (ElasticSearchClient, er
 	return ElasticSearchClient{
 		client,
 	}, nil
+}
+
+type elasticsearchObject struct {
+	Id      string  `json:"id"`
+	Name    string  `json:"name"`
+	Value   float64 `json:"value"`
+	Created int64   `json:"created"`
+}
+
+func newElasticsearchObjectFromModel(m model.Object) elasticsearchObject {
+	return elasticsearchObject{
+		Id:      m.ID.String(),
+		Name:    m.Name,
+		Value:   m.Value,
+		Created: m.Created.UnixMicro(),
+	}
+}
+
+func (es ElasticSearchClient) InsertObject(ctx context.Context, ob model.Object) (model.Object, error) {
+
+	dataJSON, err := json.Marshal(newElasticsearchObjectFromModel(ob))
+	if err != nil {
+		return model.Object{}, err
+	}
+
+	req := esapi.IndexRequest{
+		Index:      "object",
+		DocumentID: ob.ID.String(),
+		Body:       bytes.NewReader(dataJSON),
+		Refresh:    "true",
+	}
+
+	res, err := req.Do(ctx, es)
+	if err != nil {
+		return model.Object{}, err
+	}
+
+	if err != nil {
+		return model.Object{}, err
+	}
+	defer res.Body.Close()
+
+	return ob, err
 }
--- pkg/repository/elasticsearch_client_intgration_test.go
+++ pkg/repository/elasticsearch_client_intgration_test.go
@@ -1,11 +1,28 @@
 package repository
 
 import (
+	"context"
+	"grapefruit/pkg/model"
 	"testing"
 )
 
 func Test_ElasticsearchPingConnection(t *testing.T) {
 	if _, err := testElasticSearchClient.Ping(); err != nil {
 		t.Fatalf("can't ping elasticsearch: %s", err)
+	}
+}
+
+func Test_ElasticSearchIntegration_Create_Index_Object(t *testing.T) {
+
+	ctx := context.Background()
+
+	//crete
+	newObject := model.NewObject()
+	newObject.Name = "myName"
+	newObject.Value = 16
+
+	_, err := testElasticSearchClient.InsertObject(ctx, newObject)
+	if err != nil {
+		t.Fatalf("expected not error for inserting object, got: %s", err)
 	}
 }
